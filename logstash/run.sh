86cb8e3c61487886e30240a3b4ee97dceaea6043
TomonoriSoejima <tomo@elastic.co>
2025-06-18 09:54:27 +0900

    just adding some changes

--- logstash/run.sh
+++ logstash/run.sh
@@ -1,4 +1,5 @@
 docker run --rm -it \
   -v ./pipeline/:/usr/share/logstash/pipeline/ \
   -v ./input.log:/usr/share/logstash/data/input.log \
+  -v ./logstash.yml:/usr/share/logstash/config/logstash.yml \
   docker.elastic.co/logstash/logstash:9.0.0
